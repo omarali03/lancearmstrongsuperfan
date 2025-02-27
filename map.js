a5f5fe4f8fd39e01a1e0ae0292db11d225a622f6
omarali03 <omarthekid4@gmail.com>
2025-02-26 22:28:50 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,10 +1,10 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-// Set your Mapbox access token
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcmFsaXVjc2QiLCJhIjoiY203bXgxeTY2MG52czJucHZwcDVqdjdleiJ9.ZuPUOfv_SejSyAiiAcrMfA';
 
-// Initialize the map
 const map = new mapboxgl.Map({
   container: 'map', // ID of the div where the map will render
   style: 'mapbox://styles/mapbox/streets-v12', // Map style
@@ -14,7 +14,6 @@ const map = new mapboxgl.Map({
   maxZoom: 18 // Maximum allowed zoom
 });
 
-// Wait for the map to load before adding data
 map.on('load', async () => {
   // Add the Boston bike lane data source
   map.addSource('boston_route', {
@@ -22,7 +21,6 @@ map.on('load', async () => {
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
   });
 
-  // Add a layer for Boston bike lanes
   map.addLayer({
     id: 'bike-lanes-boston',
     type: 'line',
@@ -51,4 +49,16 @@ map.on('load', async () => {
       'line-opacity': 0.6
     }
   });
+  try {
+    const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+    
+    // Await JSON fetch
+    const jsonData = await d3.json(jsonurl);
+    let stations = jsonData.data.stations;
+    console.log('Stations Array:', stations); // Log stations array
+    
+  } catch (error) {
+    console.error('Error loading JSON:', error); // Handle errors
+  }
+
 });
