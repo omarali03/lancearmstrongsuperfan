12e0c11a20354ac6f74279ca88dfad93532c6885
omarali03 <omarthekid4@gmail.com>
2025-02-26 22:07:47 -0800

    Update map.js

--- map.js
+++ map.js
@@ -3,9 +3,12 @@ console.log("Mapbox GL JS Loaded:", mapboxgl);
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcmFsaXVjc2QiLCJhIjoiY203bXgxeTY2MG52czJucHZwcDVqdjdleiJ9.ZuPUOfv_SejSyAiiAcrMfA';
 
+// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', 
-  style: 'mapbox://styles/mapbox/streets-v11', 
-  center: [-71.0589, 42.3601], 
-  zoom: 12 
-});
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Basemap style
+    center: [-71.0589, 42.3601], // Center on Boston (Longitude, Latitude)
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum zoom level
+    maxZoom: 18 // Maximum zoom level
+  });
